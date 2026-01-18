696f2232eb5f0542322e94a95b3b4c788100070b
Shubh Tandon <shubhtandon802@gmail.com>
2026-01-18 21:52:17 +0530

    Removed CORS error

--- src/main/java/in/lifehive/ecom_proj/controller/ProductController.java
+++ src/main/java/in/lifehive/ecom_proj/controller/ProductController.java
@@ -2,7 +2,7 @@ package in.lifehive.ecom_proj.controller;
 
 import in.lifehive.ecom_proj.model.Product;
 import in.lifehive.ecom_proj.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
