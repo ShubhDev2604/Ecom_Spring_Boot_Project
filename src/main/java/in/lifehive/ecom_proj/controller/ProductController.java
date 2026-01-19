7e75efe7877d40a8ad3e0d7d0993c1579f3375e6
Shubh Tandon <shubhtandon802@gmail.com>
2026-01-20 00:01:36 +0530

    used response entity

--- src/main/java/in/lifehive/ecom_proj/controller/ProductController.java
+++ src/main/java/in/lifehive/ecom_proj/controller/ProductController.java
@@ -2,6 +2,8 @@ package in.lifehive.ecom_proj.controller;
 
 import in.lifehive.ecom_proj.model.Product;
 import in.lifehive.ecom_proj.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +18,25 @@ public class ProductController {
         this.service = service;
     }
 
-    @GetMapping("/")
-    public String hii(){
-        return "Hello World!";
-    }
-
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> products = service.getAllProducts();
+        if(products.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        }
     }
 
     @GetMapping("/product/{prodId}")
-    public Product getProductById(@PathVariable Long prodId) {
-        return service.getProductById(prodId);
+    public ResponseEntity<Product> getProductById(@PathVariable Long prodId) {
+        Product product = service.getProductById(prodId);
+        if(product == null) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(null);
+        } else {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
     }
 }
--- src/main/java/in/lifehive/ecom_proj/service/ProductService.java
+++ src/main/java/in/lifehive/ecom_proj/service/ProductService.java
@@ -20,6 +20,6 @@ public class ProductService {
     }
 
     public Product getProductById(Long prodId) {
-        return repo.findById(prodId).orElse(new Product());
+        return repo.findById(prodId).orElse(null);
     }
 }
